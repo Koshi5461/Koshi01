f7e874f978a935ef0d445521fb61d47897db6302
Sayak Biswas <sayak90@gmail.com>
2017-11-11 20:00:27 -0500

    Added landing scene

---
+++ Assets/LoadPracticeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPracticeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        SceneManager.LoadScene(2);
+    }
+}
