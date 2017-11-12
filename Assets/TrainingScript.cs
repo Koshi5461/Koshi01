3d27be51d1ff6ba2b65b84e7eec979ceff38f2ed
mohanzalake <mohanzalake123@gmail.com>
2017-11-11 20:16:55 -0500

    Added text box in training

---
+++ Assets/TrainingScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingScript : MonoBehaviour {
+
+    string welcomeMessage = "Hello, we will be learning how to perform CPR today. Please touch this box to continue.";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
